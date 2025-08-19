1310accb8bd524efa57b5c3574166138cdc75ecc
goopletdev <edmbeech@gmail.com>
2025-08-19 03:57:43 -0400

    restructured program, added notes to readme and comments to gitignore

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # clover
-C implementation of Plover's simplest functionality 
+Lightweight C implementation of Plover's simplest functionality 
+
+## dependencies
+in `test` directory:
+```
+git clone https://github.com/ThrowTheSwitch/Unity.git
+git pull
+```
+
+in `assets` directory:
+```
+https://raw.githubusercontent.com/aerickt/steno-dictionaries/refs/heads/main/lapwing-base.json
+```
+
+### notes to self
+Document code with Doxygen tags/Javadoc-style doc tags
+
+Consider incorporating https://zserge.com/jsmn/ for json parsing
--- clvrlib.h
+++ clvrlib.h
@@ -3,62 +3,14 @@
 //for file handling:
 #define MAX_LINE_LEN 256
 //steno consts:
-#define STENO_ORDER "#STKPWHRAO*EUFRPBLGTSDZ"
-#define MIDDLE_START 8
-#define RIGHT_START 13
 
-extern const int STENO_ORDER_SIZE;
-extern const unsigned int L_KEY_MASK;
-extern const unsigned int M_KEY_MASK;
-extern const unsigned int R_KEY_MASK;
-extern const unsigned int CANCEL_MASK;
-extern const unsigned int SEND_MASK;
-extern const unsigned int STENO_MASK;
 extern const unsigned int QWERTY_KEY_VALS[];
-
-struct dictT {
-    unsigned int id;
-    char* translation;
-    struct dictT* parent;
-    struct dictmapT* children;
-};
-
-struct dictmapT {
-    int size;
-    int capacity;
-    struct dictT** dicts;
-};
-
-typedef struct dictT dict;
-typedef struct dictmapT dictmap;
 
 typedef struct uint_arr {
     unsigned int *arr;
     int size;
 } uint_arr;
 
-struct config {
-    dict* dictionary;
-    char** listeners;
-};
-
-struct stroke {
-    unsigned int chord;
-    unsigned int was_deleted;
-    char* translation;
-};
-
-int has(dictmap* m, unsigned int id);
-int hash_index_of(dictmap* m, unsigned int id);
-dictmap* scale_up_dictmap(dictmap* old);
-dict* init_dict(unsigned int id, char* translation, dict* parent);
-dictmap* init_dictmap(int capacity);
-dict* get(dictmap* m, unsigned int id);
-dictmap* add(dictmap* m, dict* element);
-dictmap* scale_up_dictmap(dictmap* old);
-dict* push_entry(dict* d, uint_arr id, int id_pos, char *translation);
-
-void check_memory_allocation(void* ptr, int exit_code);
 void print_bits(unsigned int arr);
 
 char* pretty_chord(unsigned int chord);
