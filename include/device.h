c5cadf458175d5e83a4684cc57341a460ea11d2c
goopletdev <edmbeech@gmail.com>
2025-08-28 21:11:49 -0400

    remove bitmap declarations from device.h

--- include/device.h
+++ include/device.h
@@ -1,14 +1,12 @@
-/*#ifndef CLOVER_DEVICE_H
+#ifndef CLOVER_DEVICE_H
 #define CLOVER_DEVICE_H
 
-typedef struct clover__bitmapT clover_bitmap;
+#include "bitmap.h"
+
 typedef struct clover__deviceT clover_device;
 typedef struct clover__device_listT clover_device_list;
 
-int clover_free_bitmap(clover_bitmap m);
 int clover_free_device(clover_device d);
 
-clover_bitmap clover_parse_hexmap(char* map);
-
 #endif // CLOVER_DEVICE_H
-*/
+
