b352b93e66fafaafa1feb50f34bdfa8169345e9b
goopletdev <edmbeech@gmail.com>
2025-08-30 22:26:10 -0400

    implement toml parsing

--- Makefile
+++ Makefile
@@ -1,6 +1,4 @@
-# GNU C Compiler
 CC = gcc
-# C compiler flags:
 # -Wall: enable all common warnings;
 # -Wextra: enable extra warning flags
 # -Wno-override-init: disable warnings about struct
@@ -20,21 +18,27 @@ endif
 SRCDIR = src
 INCDIR = include
 BUILDDIR = build
-# all conventional names so far. below uses 'variable expansion syntax'
+LIBDIR = lib
+
 OBJDIR = $(BUILDDIR)/obj
 BINDIR = $(BUILDDIR)/bin
 DEPDIR = $(BUILDDIR)/deps
 
+TOML_DIR = $(LIBDIR)/tomlc17
+TOML_SRC = $(TOML_DIR)/src/tomlc17.c
+TOML_INC = $(TOML_DIR)/src
+TOML_DEP = $(DEPDIR)/tomlc17.d
+
 TESTDIR = tests
 TESTBINDIR = $(BUILDDIR)/test-bin
-UNITY_DIR = lib/Unity
+
+UNITY_DIR = $(LIBDIR)/Unity
 UNITY_SRC = $(UNITY_DIR)/src/unity.c
 UNITY_INC = $(UNITY_DIR)/src
 
 # #####################
 # # find source files #
 # #####################
-# $(shell ...) executes a shell command and captures output
 # find: recursively finds all .c files under SRCDIR
 SOURCES = $(shell find $(SRCDIR) -name "*.c")
 # pattern substitution: $(var:pattern=replacement)
@@ -47,11 +51,15 @@ TEST_OBJECTS = $(patsubst $(TESTDIR)/%.c,$(OBJDIR)/%.o,$(TEST_SOURCES))
 TEST_DEPS = $(patsubst $(TESTDIR)/%.c,$(DEPDIR)/%.d,$(TEST_SOURCES))
 TEST_BINARIES = $(patsubst $(TESTDIR)/%.c,$(TESTBINDIR)/%,$(TEST_SOURCES))
 
+TOML_OBJ = $(OBJDIR)/tomlc17.o
+OBJECTS += $(TOML_OBJ)
+
 # ##################
 # # compiler flags #
 # ##################
 # pkg-config --cflags $(PKGS) gets compiler flags needed for specified packages
 CFLAGS += -I$(INCDIR) $(shell pkg-config --cflags $(PKGS))
+CFLAGS += -I$(TOML_INC)
 # conventional variable for linker flags:
 # shell command gets library linking flags for packages
 LDFLAGS += $(shell pkg-config --libs $(PKGS))
@@ -99,6 +107,11 @@ $(TESTBINDIR)/%: $(TESTDIR)/%.c $(filter-out $(OBJDIR)/main.o, $(OBJECTS)) | $(T
 	@mkdir -p $(dir $(DEPDIR)/$*)
 	$(CC) $(CFLAGS) -I$(UNITY_INC) $(DEPFLAGS) -o $@ $< $(UNITY_SRC) $(filter-out $(OBJDIR)/main.o, $(OBJECTS)) $(LDFLAGS)
 
+$(TOML_OBJ): $(TOML_SRC) | $(OBJDIR)
+	@mkdir -p $(dir $@)
+	@mkdir -p $(dir $(TOML_DEP))
+	$(CC) $(CFLAGS) -MMD -MP -MF $(TOML_DEP) -c $< -o $@
+
 test-build: $(TEST_BINARIES)
 
 test: test-build
@@ -117,6 +130,7 @@ $(OBJDIR) $(BINDIR) $(DEPDIR) $(TESTBINDIR):
 # Make directive; executes immediately upon makefile parsing
 # include dependency files
 -include $(DEPS)
+-include $(TOML_DEP)
 
 run: $(BINDIR)/$(TARGET)
 	./$(BINDIR)/$(TARGET)
--- src/main.c
+++ src/main.c
@@ -10,8 +10,9 @@
 #include <unistd.h>
 #include <libevdev/libevdev-uinput.h>
 
-#define EVENT_PATH "/dev/input/event23"
-#define DICTIONARY_PATH "./dictionaries/lapwing-base.json"
+#include "tomlc17.h"
+
+const int BUFFER_SIZE = 1024;
 
 void handle_cli_args(int argc, char **argv) {
     printf("Running ");
@@ -45,14 +46,74 @@ void clover__event_loop(int fd, struct libevdev_uinput* uinput_dev, clover_dict*
     }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
+    /******************************************************
+     * ARGS
+     *****************************************************/
+    // placeholder to avoid compiler warnings until later implementation
     handle_cli_args(argc, argv);
 
-    int kbd_fd = open(EVENT_PATH, O_RDONLY);
-    if (kbd_fd == -1) {
-        printf("Error opening event handler\n");
+    /******************************************************
+     * CONFIG PARSING
+     *****************************************************/
+    toml_result_t result = toml_parse_file_ex("config.toml");
+    if (!result.ok) {
+        printf("Could not parse .toml\n");
         exit(1);
     }
+    printf("Dictionary path: %s\n", dict_path.u.str.ptr);
+
+    /******************************************************
+     * DICTIONARY PARSING
+     *****************************************************/
+    // get dictionary path
+    toml_datum_t dict_path = toml_seek(result.toptab, "dictionary.path");
+    if (dict_path.type != TOML_STRING) {
+        printf("missing or invalid 'dictionary.path' property in config\n");
+        exit(1);
+    }
+
+    // get array of dictionary paths
+    toml_datum_t dict_array = toml_seek(result.toptab, "dictionary.dictionaries");
+    if (dict_array.type != TOML_ARRAY) {
+        printf("missing or invalid 'dictionary.dictionaries' property in config");
+        exit(1);
+    }
+
+    // initialize clover dictionary
+    clover_dict* d = clover_init_dict(0, NULL, NULL);
+
+    // parse each dictionary into single dictionary object
+    for (int i = 0; i < dict_array.u.arr.size; i++) {
+        toml_datum_t elem = dict_array.u.arr.elem[i];
+        if (elem.type != TOML_STRING) {
+            printf("dictionary.dictionaries element is not a string");
+            exit(1);
+        }
+        printf("dictionary [%i]: %s\n", i, elem.u.str.ptr);
+        char buffer[BUFFER_SIZE] = {'\0'};
+        strcpy(buffer, dict_path);
+        strcat(buffer, file_name);
+        d = clover_parse_dictionary(buffer, d);
+    }
+    printf("Finished parsing dictionary. Size: %i\n", clover_dict_size(d));
+
+    /******************************************************
+     * KEYBOARD EVENT
+     *****************************************************/
+    // get event path
+    toml_datum_t event_path_toml = toml_seek(result.toptab, "keyboard.path");
+    if (event_path.type != TOML_STRING) {
+        printf("missing or invalid 'keyboard.path' property in config\n");
+        exit(1);
+    }
+    // get file descriptor
+    int kbd_fd = open(event_path_toml.u.str.ptr, O_RDONLY);
+    if (kbd_fd == -1) {
+        printf("Error opening event %s\n", event_path_toml.u.str.ptr);
+        exit(1);
+    }
+
     struct libevdev *kbd_dev;
     libevdev_new_from_fd(kbd_fd, &kbd_dev);
 
@@ -60,22 +121,30 @@ int main(int argc, char **argv) {
     int uinput_fd = open("/dev/uinput", O_RDWR);
     libevdev_uinput_create_from_device(kbd_dev, uinput_fd, &virtkbd_dev);
     const char *virtkbd_path = libevdev_uinput_get_devnode(virtkbd_dev);
-    printf("Virtual keyboard device: %s\n", virtkbd_path);
-
-
-    printf("Parsing dictionary...\n");
-    clover_dict* dict = clover_parse_dictionary(DICTIONARY_PATH);
-    if (dict == NULL) {
-        printf("Error parsing dictionary\n");
-        exit(1);
-    }
-    printf("Finished parsing dictionary. Size: %i\n", clover_dict_size(dict));
+    printf("Virtual keyboard device %s\n", virtkbd_path);
 
     int grab = 1;
     ioctl(kbd_fd, EVIOCGRAB, &grab);
 
-    clover__event_loop(kbd_fd, virtkbd_dev, dict);
 
+
+    /******************************************************
+     * TOML CLEANUP
+     *****************************************************/
+    // free toml object
+    toml_free(result);
+
+    /******************************************************
+     * MAIN LOOP
+     *****************************************************/
+    clover__event_loop(kbd_fd, virtkbd_dev, d);
+
+    /******************************************************
+     * CLEANUP
+     *****************************************************/
     close(kbd_fd);
+    close(uinput_fd);
+
     return 0;
 }
+
