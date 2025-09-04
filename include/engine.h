8042610d10c6ad90ea2eedd0ff9ec25363a256a2
goopletdev <edmbeech@gmail.com>
2025-09-04 16:31:49 -0400

    start implementing history elements by splitting dictionary translations into array of 'instruction' types

--- include/engine.h
+++ include/engine.h
@@ -5,39 +5,65 @@
 #include "table.h"
 #include "bitmap.h"
 
-typedef struct clover__historyT clover_history;
-struct clover__historyT {
+typedef struct clover__instructionT clover_instruction;
+// i don't think deletedText will work correctly with unicode
+// figure that out later ig
+struct clover__instructionT {
     enum {
-        INPUT, COMMAND,
-    } chordType;
-    struct {
+        NONE, ASCII, UNICODE, COMMAND, DELETE, 
+    } type;
+    union {
         char* inputText;
         char* deletedText;
+        char* keyCombo;
+        int* unicode;
+    } u;
+};
+
+typedef struct clover__historyT clover_history;
+struct clover__historyT {
+    struct {
+        clover_instruction* instructions;
+        int instructions_len;
         clover_dict* source_dict;
     } translation;
-    struct clover__historyT* previous;
+    struct clover__historyT* prev;
     struct clover__historyT* next;
 };
 
 typedef struct clover__instanceT clover_instance;
 struct clover__instanceT {
-    struct libevdev_uinput* virtkb_dev;
+    enum {
+        STENO, PASSTHROUGH,
+    } mode;
+    struct {
+        enum {
+            BEFORE, AFTER,
+        } space_position;
+        int space_adjustment;
+    } input_settings;
+
+
+    struct {
+        struct libevdev_uinput* virtkb_dev;
+        clover_bitmap state;
+    } virtual_kbd;
     struct {
         int kbd_fd;
         clover_bitmap keys;
         clover_bitmap state;
     } keyboard;
     struct {
-        int max_size;
-        int current_size;
-        clover_history* first;
-        clover_history* current;
+        int max_len;
+        int current_len;
+        clover_history* head;
+        clover_history* tail;
     } history;
     clover_dict* root;
 };
 
 void clover_free_history(clover_history* h);
 
-void clover_instance_push_history(clover_instance* ci, clover_history new);
+void clover_instance_push_history(clover_instance* ci, clover_history* new);
 
 #endif // CLOVER_ENGINE_H
--- src/engine.c
+++ src/engine.c
@@ -1,18 +1,86 @@
 #include "engine.h"
 
+void clover_free_instruction(clover_instruction* i) {
+    switch (i->type) {
+        case (ASCII): 
+            free(i->u.inputText);
+        case (UNICODE):
+            free(i->u.unicode);
+        case (COMMAND):
+            free(i->u.keyCombo);
+        case (DELETE):
+            free(i->u.deletedText);
+    }
+    free(i);
+}
+
 void clover_free_history(clover_history* h) {
     if (!h) return;
-    free(h->translation.inputText);
-    free(h->translation.deletedText);
-    clover_free_history(h->previous);
+    if (h->translation.instructions) {
+        for (int i = 0; i < h->translation.instructions_len; i++) {
+            clover_free_instruction(&h->translation.instructions[i]);
+        }
+        free(h->translation.instructions);
+    }
     free(h);
 }
 
-void clover_instance_push_history(clover_instance* ci, clover_history new) {
-    if (++ci->history.current_size >= ci->history.max_size) {
-        ci->history.first = ci->history.first->next;
-        clover_free_history(ci->history.first->previous);
-        ci->history.current_size--;
+void clover_free_history_recursive(clover_history* h) {
+    if (h->prev) {
+        clover_free_history_recursive(h->prev);
     }
-    // figure out how to implement this after you sleep a little
+    clover_free_history(h);
+}
+
+void clover_instance_push_history(clover_instance* ci, clover_history* new) {
+    if (++ci->history.current_len >= ci->history.max_len) {
+        ci->history.head = ci->history.head->next;
+        clover_free_history_recursive(ci->history.head->prev);
+        ci->history.current_len--;
+    }
+    ci->history.tail->next = new;
+    ci->history.tail = ci->history.tail->next;
+}
+
+char parse_escaped_char(char c) {
+    switch (c) {
+        case ('t'):
+            return '\t';
+        case ('b'):
+            return '\b';
+        case ('n'):
+            return '\n';
+        default:
+            return c;
+    }
+}
+
+clover_history* clover_history_from_dict(clover_instance* ci, clover_dict* dict) {
+    clover_history* new = (clover_history*)malloc(sizeof(clover_history));
+    new.translation.instructions = null;
+    new.translation.instructions_len = 0;
+    new.translation.source_dict = dict;
+
+    clover_instruction* instruction = (clover_history*)malloc(sizeof(clover_instruction));
+
+    int is_escaped = 0;
+    int buffer_len = 0;
+    char buffer[1024] = { '\0' };
+    if (!dict->translations.entries) {
+        instruction.type = NONE;
+        new.translation.instructions = instruction;
+        return new;
+    }
+    char c;
+    for (int i = 0; (c = dict->translations.entries[i]) != '\0'; i++) {
+        if (is_escaped) {
+            is_escaped = 0;
+            buffer[buffer_len++] = parse_escaped_char(c);
+        } else if (c == '{') {
+            if (buffer_len) {
+                instruction
+
+
+        
+    
 }
